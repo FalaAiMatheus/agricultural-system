18269b770196411392f6e84fbf0ba861daa87625
FalaAiMatheus <matheussfrancads2345@gmail.com>
2025-10-15 11:53:32 -0300

    chore: changing exception message

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -3,17 +3,22 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (AccessDeniedHttpException $e) {
+            return response()->json([
+                'message' => 'Você não tem permissão'
+            ], 403);
+        });
     })->create();
