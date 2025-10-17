112560fc33d984056c2266e3744b3b33459a99e8
FalaAiMatheus <matheussfrancads2345@gmail.com>
2025-10-17 09:52:46 -0300

    feat: add docker ambient

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,4 +5,8 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+  },
 });
